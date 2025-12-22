d182f40f34e7db982e7e5e2386fbfc39fe312fd6
f-sato-codes <bluekill4649@gmail.com>
2025-12-22 16:30:30 +0900

    Add initial transactions index view

---
+++ resources/views/transactions.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>収支一覧</title>
+</head>
+<body>
+    <h1>収支一覧</h1>
+
+    <p>※ 現在は仮画面です。</p>
+
+    <table border="1" cellpadding="8">
+        <thead>
+            <tr>
+                <th>日付</th>
+                <th>カテゴリ</th>
+                <th>金額</th>
+                <th>メモ</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>2025-01-01</td>
+                <td>食費</td>
+                <td>-1,000</td>
+                <td>スーパー</td>
+            </tr>
+            <tr>
+                <td>2025-01-02</td>
+                <td>給料</td>
+                <td>+250,000</td>
+                <td>1月分</td>
+            </tr>
+        </tbody>
+    </table>
+</body>
+</html>

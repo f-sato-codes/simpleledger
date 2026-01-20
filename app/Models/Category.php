82b5e18fad528001bc2552ecb8d8437bdfa529f7
fumiharu <bluekill4649@gmail.com>
2026-01-20 14:20:09 +0900

    Categoryにtypeを追加（fillable修正）

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['user_id', 'category_name'];
+    protected $fillable = ['user_id', 'category_name','type'];
 
     public function transactions()
     {
